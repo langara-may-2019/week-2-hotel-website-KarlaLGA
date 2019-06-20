8b1c2122a014bad9f6adf94ee6c1c1f9f152b67e
Trista Townsend <tristatownsend@gmail.com>
2019-06-19 18:54:40 -0700

    updated .js file

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-$('nav li').hover(
-    function() {
-      $('ul', this).stop().slideDown(200);
-    },
-    function() {
-      $('ul', this).stop().slideUp(200);
-    }
-  );
+// 1. Let's make a dropdown menu, by starting by selecting the 'about' nav li
+// 2. Next, use the event of 'hover' by either using .on("hover", function...) or .hover(function...).
+// 3. Next, add a callback function. 
+// 4. select the ul and look into what you want the ul to do. Do a google search, and if you get stuck, look into what .stop() does, and what .slideDown() does. Look them up and see what they do. This is 95% of what developers do during their day. 
+// 5. So we animated it to pop down, but now we need it to pop back up. Find out what the opposite of .slideDown is, and put that in your code.
+// 6. STRETCH - try making this a different way. Maybe you could make it dissapear with a jquery function. The li is hidden by using the CSS display: none;. Remove the CSS, and use jquery to hide and reveal the menu. Or perhaps you could toggle a class on and off. The possibilities are endless!  
+
+
+// Example  👇 
+// $('').on(" ",function() {
+//       $('', this).something().something();
+//     },
+//     function() {
+//       $('', this).something().something();
+//     }
+//   );
